5158953e4dd4fde03b15b5312efb93252c3f7da8
Nass <nasali@anypresence.com>
2014-06-02 15:28:25 -0400

    Switch to release path for library var setting.

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -34,7 +34,12 @@ class LanguagePack::Ruby < LanguagePack::Base
   FREETDS_TGZ_URL="#{CHAMELEON_S3_BUCKET}/freetds.tar.gz"
   FREETDS_DIR_ABSOLUTE_PATH = "#{ARGV[0]}/vendor/freetds"
   FREETDS_DIR_FOR_RELEASE = "#{ENV['HOME']}/vendor/freetds"
-    
+
+  SAP_HANA_TRIGGER = '.odbc.ini'
+  UNIX_ODBC_WITH_HANA_TGZ_URL = "#{CHAMELEON_S3_BUCKET}/unixodbc.tar.gz"
+  UNIX_ODBC_DIR_ABSOLUTE_PATH = "#{ARGV[0]}/vendor/unixodbc"
+  UNIX_ODBC_DIR_FOR_RELEASE = "#{ENV['HOME']}/vendor/unixodbc"
+  
   # detects if this is a valid Ruby app
   # @return [Boolean] true if it's a Ruby app
   def self.use?
@@ -537,17 +542,44 @@ WARNING
   def uses_freetds?
     File.exist?(File.join(Dir.pwd,FREETDS_TRIGGER_NAME))
   end
+
+  def uses_sap_hana?
+    File.exist?(File.join(Dir.pwd,SAP_HANA_TRIGGER))
+  end
+    
+  def install_sap_hana_binaries
+    
+    `mkdir -p #{UNIX_ODBC_DIR_ABSOLUTE_PATH}` unless Dir.exists?(UNIX_ODBC_DIR_ABSOLUTE_PATH)
+    `mkdir -p #{UNIX_ODBC_DIR_FOR_RELEASE}` unless Dir.exists?(UNIX_ODBC_DIR_FOR_RELEASE)
+    
+    result = `curl #{UNIX_ODBC_WITH_HANA_TGZ_URL} -s -o - | tar -xz -C #{UNIX_ODBC_DIR_ABSOLUTE_PATH} -f - `
+    if $?.success?
+      puts "Setting SAP HANA environment variables"
+      ENV["LD_LIBRARY_PATH"]="#{UNIX_ODBC_DIR_ABSOLUTE_PATH}:#{ENV['LD_LIBRARY_PATH']}" # Required for ruby odbc gem
+      puts "Done installing SAP HANA binaries"
+    else
+      raise "Failed to install SAP HANA binaries"
+    end
+  end
   
   def build_native_gems
     puts "Building native gems..."
+    
     if uses_oci8?
       puts "Found OCI8 trigger"
       install_oci8_binaries 
     end
+    
     if uses_freetds?
       puts "Found FreeTDS trigger"
       install_freetds_binaries
     end
+    
+    if uses_sap_hana?
+      puts "Found SAP HANA trigger"
+      install_sap_hana_binaries
+    end
+    
     puts "Done building native gems."
   end
   
