dc304b7c1e5dbcb092ee98ae21b11c9c072f2d81
Nass <nasali@anypresence.com>
2014-06-18 10:49:11 -0400

    Try no dot

--- lib/language_pack/anypresence.rb
+++ lib/language_pack/anypresence.rb
@@ -23,7 +23,7 @@ module LanguagePack
       ld_library_vars = []
       
       if uses_oci8?
-        ld_library_vars << ORACLE_INSTANT_CLIENT_DIR_FOR_RELEASE
+        ld_library_vars << ORACLE_INSTANT_CLIENT_DIR
         extra_vars["NLS_LANG"] = 'AMERICAN_AMERICA.UTF8'
         `export NLS_LANG='AMERICAN_AMERICA.UTF8'`
         ENV['NLS_LANG'] = 'AMERICAN_AMERICA.UTF8'
