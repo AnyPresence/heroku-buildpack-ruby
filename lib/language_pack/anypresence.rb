37205d5a1f8393f69cf8e3b8b4e5a9f8f5220888
Nass <nasali@anypresence.com>
2014-06-18 14:46:46 -0400

    Try to use bundle config for FreeTDS instead.

--- lib/language_pack/anypresence.rb
+++ lib/language_pack/anypresence.rb
@@ -74,7 +74,7 @@ module LanguagePack
         puts "Creating Bundler configuration file for FreeTDS"
         puts "Folder has in it"
         puts `ls -alh #{FREETDS_DIR}`
-        `bundle config build.tiny_tds --with-freetds-include=#{FREETDS_DIR}/include --with-freetds-lib=#{FREETDS_DIR}/lib 2&>1`
+        `bundle config build.tiny_tds --with-freetds-dir=#{FREETDS_DIR} 2&>1`
         raise "Error configuring FreeTDS! #{$?}" unless $?.success?
       else
         raise "Failed to install FreeTDS binaries"
