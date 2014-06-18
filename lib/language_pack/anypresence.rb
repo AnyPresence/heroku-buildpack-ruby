133859ca360729c1f7bbd50c1dc39c2f19f22189
Nass <nasali@anypresence.com>
2014-06-18 11:05:21 -0400

    Try home instead

--- lib/language_pack/anypresence.rb
+++ lib/language_pack/anypresence.rb
@@ -5,8 +5,8 @@ module LanguagePack
 
     OCI8_TRIGGER_NAME = '.oracle.ini'
     ORACLE_INSTANT_CLIENT_TGZ_URL = "#{CHAMELEON_S3_BUCKET}/instantclient_11_2_with_libaio_oci8.tar.gz"
-    ORACLE_INSTANT_CLIENT_DIR = "#{ARGV[0]}/vendor/instant_client_11_2"
-    ORACLE_INSTANT_CLIENT_DIR_FOR_RELEASE = "vendor/instant_client_11_2"
+    ORACLE_INSTANT_CLIENT_DIR = "#{ENV['HOME']}/vendor/instant_client_11_2"
+    ORACLE_INSTANT_CLIENT_DIR_FOR_RELEASE = "/app/vendor/instant_client_11_2"
 
     FREETDS_TRIGGER_NAME = '.freetds.conf'
     FREETDS_TGZ_URL="#{CHAMELEON_S3_BUCKET}/freetds.tar.gz"
