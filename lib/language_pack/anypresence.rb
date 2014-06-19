53cb54f7be5bfb40cc71b4d9b6becdeb81592bd9
Nass <nasali@anypresence.com>
2014-06-19 10:39:51 -0400

    Actually set FreeTDS env variable absolutely.

--- lib/language_pack/anypresence.rb
+++ lib/language_pack/anypresence.rb
@@ -73,7 +73,7 @@ module LanguagePack
       result = `curl #{FREETDS_TGZ_URL} -s -o - | tar -xz -C #{FREETDS_DIR} -f - `
       if $?.success?
         puts "Setting environment variable for FreeTDS"
-        ENV["FREETDS_DIR"] = FREETDS_DIR
+        ENV["FREETDS_DIR"] = FREETDS_DIR_FOR_RELEASE
       else
         raise "Failed to install FreeTDS binaries"
       end
